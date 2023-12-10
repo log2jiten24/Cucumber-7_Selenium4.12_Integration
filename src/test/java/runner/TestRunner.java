47549abc308c455333c33085f08e7c2340833dcc
KumarJitendra <=>
2023-12-10 18:43:38 +1100

    12 Dec_Cucumber_07_Selenium_latest_4.12_Integration

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,21 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
+
+
+@CucumberOptions(features = "src/test/resources/features",
+        glue = {"com.qa.stepdefinitions"},
+        plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"},
+        monochrome = true,
+        publish = true)
+//to run the tests in parallel mode
+public class TestRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
